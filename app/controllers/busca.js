f550678643ade18e958f936e93c353eb1f7b4bb1
BrotherPool <marcoslevi1213@gmail.com>
2017-06-29 20:57:37 -0300

    Apaguei a pasta busca

--- app/controllers/busca.js
+++
@@ -1,6 +0,0 @@
-module.exports.busca=function(application,req,res){
-	var noticia=req.body;
-	//req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD' });
-	res.render("busca/teste");	
-
-}
--- app/routes/busca.js
+++
@@ -1,9 +0,0 @@
-//var dbConnection =require ('../../config/dbConnection');
-module.exports=function(application){
-		
-	application.get('/busca2',function(req,res){
-
-		application.app.controllers.busca.busca(application,req,res);
-		
-	});
-};
